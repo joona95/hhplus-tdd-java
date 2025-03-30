83820f13ab130ea3af63a39fb9da49ace1bc9eb5
joona95 <choijuna052@gmail.com>
2025-03-30 19:31:09 +0900

    test: PointService 단위테스트 테스트네이밍 변경 및 실패테스트 추가

--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -35,7 +35,7 @@ class PointServiceTest {
     class 유저_포인트_조회 {
 
         @Test
-        void 특정_유저_아이디_값으로_포인트_조회_시_정상적으로_해당_유저_포인트_반환() {
+        void 특정_유저_포인트_정상적으로_반환() {
 
             //given
             when(userPointTable.selectById(1L))
@@ -47,13 +47,23 @@ class PointServiceTest {
             //then
             assertThat(result).isEqualTo(new UserPoint(1L, ANY_AMOUNT, ANY_UPDATE_MILLIS));
         }
+
+        @Test
+        void 특정_유저_포인트_조회_정상적으로_요청() {
+
+            //when
+            pointService.getUserPointById(1L);
+
+            //then
+            verify(userPointTable, times(1)).selectById(1L);
+        }
     }
 
     @Nested
     class 포인트_내역_조회 {
 
         @Test
-        void 특정_유저_아이디_값으로_포인트_충전_사용_내역_조회_시_정상적으로_포인트_충전_사용_내역_목록_반환() {
+        void 특정_유저_포인트_내역_정상적으로_반환() {
 
             //given
             List<PointHistory> pointHistories = List.of(
@@ -70,11 +80,17 @@ class PointServiceTest {
 
             //then
             assertThat(result).hasSize(3);
-            assertThat(result).isEqualTo(List.of(
-                    new PointHistory(1L, 1L, ANY_AMOUNT, ANY_TRANSACTION_TYPE, ANY_UPDATE_MILLIS),
-                    new PointHistory(2L, 1L, ANY_AMOUNT, ANY_TRANSACTION_TYPE, ANY_UPDATE_MILLIS),
-                    new PointHistory(3L, 1L, ANY_AMOUNT, ANY_TRANSACTION_TYPE, ANY_UPDATE_MILLIS)
-            ));
+            assertThat(result).isEqualTo(pointHistories);
+        }
+
+        @Test
+        void 특정_유저_포인트_내역_조회_정상적으로_요청() {
+
+            //when
+            pointService.getPointHistoriesByUserId(1L);
+
+            //then
+            verify(pointHistoryTable, times(1)).selectAllByUserId(1L);
         }
     }
 
@@ -82,15 +98,48 @@ class PointServiceTest {
     class 포인트_충전 {
 
         @Test
-        void 특정_유저_아이디에_특정_금액만큼_포인트_충전_시_해당_유저_포인트_잔고에_충전_금액_더해진_유저_포인트_반환() {
+        void 유저_포인트_조회_실패_시_유저_포인트_저장과_포인트_내역_저장_요청이_발생하지_않음() {
 
             //given
+            when(userLockManager.getLock(1L))
+                    .thenReturn(new ReentrantLock());
+            when(userPointTable.selectById(1L))
+                    .thenThrow(new RuntimeException());
+
+            //when, then
+            assertThatThrownBy(() -> pointService.charge(1L, 1000L))
+                    .isInstanceOf(RuntimeException.class);
+            verify(userPointTable, times(0)).insertOrUpdate(1L, ANY_AMOUNT + 1000L);
+            verify(pointHistoryTable, times(0)).insert(1L, 1000L, TransactionType.CHARGE, ANY_UPDATE_MILLIS);
+        }
+
+        @Test
+        void 유저_포인트_저장_실패_시_포인트_내역_저장_요청이_발생하지_않음() {
+
+            //given
+            when(userLockManager.getLock(1L))
+                    .thenReturn(new ReentrantLock());
+            when(userPointTable.selectById(1L))
+                    .thenReturn(new UserPoint(1L, 1000L, ANY_UPDATE_MILLIS));
+            when(userPointTable.insertOrUpdate(1L, 2000L))
+                    .thenThrow(new RuntimeException());
+
+            //when, then
+            assertThatThrownBy(() -> pointService.charge(1L, 1000L))
+                    .isInstanceOf(RuntimeException.class);
+            verify(pointHistoryTable, times(0)).insert(1L, 1000L, TransactionType.CHARGE, ANY_UPDATE_MILLIS);
+        }
+
+        @Test
+        void 특정_유저_포인트_잔고에_충전_금액_더해진_유저_포인트_반환() {
+
+            //given
+            when(userLockManager.getLock(1L))
+                    .thenReturn(new ReentrantLock());
             when(userPointTable.selectById(1L))
                     .thenReturn(new UserPoint(1L, 1000L, ANY_UPDATE_MILLIS));
             when(userPointTable.insertOrUpdate(1L, 2000L))
                     .thenReturn(new UserPoint(1L, 2000L, ANY_UPDATE_MILLIS));
-            when(userLockManager.getLock(1L))
-                    .thenReturn(new ReentrantLock());
 
             //when
             UserPoint result = pointService.charge(1L, 1000L);
@@ -100,7 +149,7 @@ class PointServiceTest {
         }
 
         @Test
-        void 특정_유저_아이디에_특정_금액만큼_포인트_충전_시_정상적으로_유저_포인트_업데이트와_포인트_충전_내역_저장() {
+        void 유저_포인트_업데이트와_포인트_내역_저장_정상적으로_요청() {
 
             //given
             when(userPointTable.selectById(1L))
@@ -123,39 +172,72 @@ class PointServiceTest {
     class 포인트_사용 {
 
         @Test
-        void 특정_유저_아이디에_특정_금액만큼_포인트_사용_시_해당_유저_포인트_잔고에_충전_금액_뺀_유저_포인트_반환() {
+        void 유저_포인트_조회_실패_시_유저_포인트_저장과_포인트_내역_저장_요청이_발생하지_않음() {
 
             //given
-            when(userPointTable.selectById(1L))
-                    .thenReturn(new UserPoint(1L, 1000L, ANY_UPDATE_MILLIS));
-            when(userPointTable.insertOrUpdate(1L, 0L))
-                    .thenReturn(new UserPoint(1L, 0L, ANY_UPDATE_MILLIS));
             when(userLockManager.getLock(1L))
                     .thenReturn(new ReentrantLock());
+            when(userPointTable.selectById(1L))
+                    .thenThrow(new RuntimeException());
 
-            //when
-            UserPoint result = pointService.use(1L, 1000L);
-
-            //then
-            assertThat(result).isEqualTo(new UserPoint(1L, 0L, result.updateMillis()));
+            //when, then
+            assertThatThrownBy(() -> pointService.use(1L, 1000L))
+                    .isInstanceOf(RuntimeException.class);
+            verify(userPointTable, times(0)).insertOrUpdate(1L, ANY_AMOUNT - 1000L);
+            verify(pointHistoryTable, times(0)).insert(1L, 1000L, TransactionType.USE, ANY_UPDATE_MILLIS);
         }
 
         @Test
-        void 특정_유저_아이디에_특정_금액만큼_포인트_사용_시_정상적으로_유저_포인트_업데이트와_포인트_충전_내역_저장() {
+        void 유저_포인트_저장_실패_시_포인트_내역_저장_요청이_발생하지_않음() {
 
             //given
-            when(userPointTable.selectById(1L))
-                    .thenReturn(new UserPoint(1L, 1000L, ANY_UPDATE_MILLIS));
-            when(userPointTable.insertOrUpdate(1L, 0L))
-                    .thenReturn(new UserPoint(1L, 0L, ANY_UPDATE_MILLIS));
             when(userLockManager.getLock(1L))
                     .thenReturn(new ReentrantLock());
+            when(userPointTable.selectById(1L))
+                    .thenReturn(new UserPoint(1L, 2000L, ANY_UPDATE_MILLIS));
+            when(userPointTable.insertOrUpdate(1L, 1000L))
+                    .thenThrow(new RuntimeException());
+
+            //when, then
+            assertThatThrownBy(() -> pointService.use(1L, 1000L))
+                    .isInstanceOf(RuntimeException.class);
+            verify(pointHistoryTable, times(0)).insert(1L, 1000L, TransactionType.CHARGE, ANY_UPDATE_MILLIS);
+        }
+
+        @Test
+        void 특정_유저_포인트_잔고에_사용_금액_뺀_유저_포인트_반환() {
+
+            //given
+            when(userLockManager.getLock(1L))
+                    .thenReturn(new ReentrantLock());
+            when(userPointTable.selectById(1L))
+                    .thenReturn(new UserPoint(1L, 2000L, ANY_UPDATE_MILLIS));
+            when(userPointTable.insertOrUpdate(1L, 1000L))
+                    .thenReturn(new UserPoint(1L, 1000L, ANY_UPDATE_MILLIS));
 
             //when
             UserPoint result = pointService.use(1L, 1000L);
 
             //then
-            verify(userPointTable, times(1)).insertOrUpdate(1L, 0L);
+            assertThat(result).isEqualTo(new UserPoint(1L, 1000L, result.updateMillis()));
+        }
+
+        @Test
+        void 특정_유저_포인트_업데이트와_포인트_내역_저장_정상적으로_요청() {
+
+            //given
+            when(userLockManager.getLock(1L))
+                    .thenReturn(new ReentrantLock());
+            when(userPointTable.selectById(1L))
+                    .thenReturn(new UserPoint(1L, 2000L, ANY_UPDATE_MILLIS));
+            when(userPointTable.insertOrUpdate(1L, 1000L))
+                    .thenReturn(new UserPoint(1L, 1000L, ANY_UPDATE_MILLIS));
+
+            //when
+            UserPoint result = pointService.use(1L, 1000L);
+
+            //then
+            verify(userPointTable, times(1)).insertOrUpdate(1L, 1000L);
             verify(pointHistoryTable, times(1)).insert(1L, 1000L, TransactionType.USE, result.updateMillis());
         }
     }
